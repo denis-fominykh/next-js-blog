9f42cea8e6bba9f6440380ed8185c0445a0fe301
denisfominykh <2028902d>
2022-01-08 03:30:36 +0200

    feat: create navigation bar component

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import type { ReactNode } from 'react';
+
+import { Navbar, LogoLink, Container } from '~/components/NavBar/styled';
+
+interface NavBarProps {
+  children: ReactNode;
+}
+
+export default function NavBar({ children }: NavBarProps) {
+  return (
+    <>
+      <Navbar>
+        <LogoLink href="/">Remix</LogoLink>
+        <ul>
+          <li>
+            <Link href="/people">People</Link>
+          </li>
+        </ul>
+      </Navbar>
+      <Container>{children}</Container>
+    </>
+  );
+}
