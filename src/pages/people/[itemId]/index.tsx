2481d9ec0fcd59a165426d512d2552c49abeb916
denisfominykh <2028902d>
2022-01-08 16:10:32 +0200

    feat: change improt to import type

--- src/pages/people/[itemId]/index.tsx
+++ src/pages/people/[itemId]/index.tsx
@@ -1,4 +1,4 @@
-import { ParsedUrlQuery } from 'querystring';
+import type { ParsedUrlQuery } from 'querystring';
 import type { NextPage, GetStaticPaths, GetStaticProps } from 'next';
 
 import { SWCharacter } from '~/api/types';
