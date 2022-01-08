0a4fdd41677ef5968f78449dc860ebf872c30b61
denisfominykh <2028902d>
2022-01-08 03:31:45 +0200

    feat: change home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,14 @@
-import Image from 'next/image';
 import type { NextPage } from 'next';
-
-import {
-  Main,
-  Code,
-  Grid,
-  Card,
-  Logo,
-  Title,
-  Footer,
-  Container,
-  Description,
-} from '~/pages/styled';
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <Main>
-        <Title>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Title>
-
-        <Description>
-          Get started by editing <Code>pages/index.tsx</Code>
-        </Description>
-
-        <Grid>
-          <Card href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </Card>
-
-          <Card href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </Card>
-
-          <Card href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </Card>
-
-          <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </Card>
-        </Grid>
-      </Main>
-
-      <Footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Logo>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </Logo>
-        </a>
-      </Footer>
-    </Container>
+    <div>
+      <h1>Welcome!</h1>
+      <p>
+        Next.js is a full stack web framework by Vercel. This is a simple blog
+        app to demonstrate the power of Next.js.
+      </p>
+    </div>
   );
 };
 
